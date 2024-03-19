339d98f2f9a634793512a78a51f3c32c711f7e12
Ayaan Alam <ayaanabaf@gmail.com>
2024-03-19 15:54:02 +0500

    Al-Hamd

---
+++ app.js
@@ -0,0 +1,382 @@
+function calculateMarksclass10() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+
+var totalmarks=505;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+    
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+function calculateMarksclass9() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+
+var totalmarks=565;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+function calculateMarksclass8() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+
+var totalmarks=650;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+
+function calculateMarksclass8term() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+
+var totalmarks=675;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+
+function calculateMarksclass7() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+    var subject11 = +(document.getElementById("subject11").value);
+    var subject12 = +(document.getElementById("subject12").value);
+
+var totalmarks=650;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10 + subject11 + subject12;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+
+function calculateMarksclass7term() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+    var subject11 = +(document.getElementById("subject11").value);
+    var subject12 = +(document.getElementById("subject12").value);
+
+var totalmarks=900;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10 + subject11 + subject12;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+
+function calculateMarksclass4() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+    var subject11 = +(document.getElementById("subject11").value);
+    var subject12 = +(document.getElementById("subject12").value);
+
+var totalmarks=625;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10 + subject11 + subject12;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+
+function calculateMarksclass4term() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+    var subject11 = +(document.getElementById("subject11").value);
+    var subject12 = +(document.getElementById("subject12").value);
+
+var totalmarks=850;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10 + subject11 + subject12;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+
+function calculateMarksclass2() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+    var subject11 = +(document.getElementById("subject11").value);
+    var subject12 = +(document.getElementById("subject12").value);
+
+var totalmarks=575;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10 + subject11 + subject12;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
+
+function calculateMarksclass2term() {
+    var subject1 = +(document.getElementById("subject1").value);
+    var subject2 = +(document.getElementById("subject2").value);
+    var subject3 = +(document.getElementById("subject3").value);
+    var subject4 = +(document.getElementById("subject4").value);
+    var subject5 = +(document.getElementById("subject5").value);
+    var subject6 = +(document.getElementById("subject6").value);
+    var subject7 = +(document.getElementById("subject7").value);
+    var subject8 = +(document.getElementById("subject8").value);
+    var subject9 = +(document.getElementById("subject9").value);
+    var subject10 = +(document.getElementById("subject10").value);
+    var subject11 = +(document.getElementById("subject11").value);
+    var subject12 = +(document.getElementById("subject12").value);
+
+var totalmarks=800;
+    var obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5 + subject6 + subject7 + subject8 + subject9 + subject10 + subject11 + subject12;
+    var percentage = (obtainedMarks / totalmarks) * 100;
+
+    var grade;
+    if (percentage >= 90) {
+        grade = 'A+1';
+    } else if (percentage >= 80) {
+        grade = 'A+';
+    } else if (percentage >= 70) {
+        grade = 'A';
+    } else if (percentage >= 60) {
+        grade = 'B';
+    } else if (percentage >= 50) {
+        grade = 'C';
+    } else {
+        grade = 'F';
+    }
+
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Obtained Marks: " + obtainedMarks + "<br>" +
+                          "Percentage: " + percentage + "%" + "<br>" +
+                          "Grade: " + grade;
+}
